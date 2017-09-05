f8b719cc5bd1be599669201fc60a998756acbd5d
miguel <progeek993@gmail.com>
2017-09-05 10:27:51 -0400

    primera subida

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+(function(){
+	   if (navigator.geolocation) {
+	   	navigator.geolocation.getCurrentPosition(getCords, errorFound);
+	   }else{
+	   	alert("Por favor, actualiza tu navegador.");
+	   }
+
+
+	   function errorFound(error) {
+	   		alert("Un error ocurrió" + error.code);
+	   		// 0: Error desconocido
+	   		// 1: Permiso denegado
+	   		// 2: Posición no está disponible
+	   		// 3: Timeout
+	   };
+
+	   function getCords(position){
+	   		var lat = position.coords.latitude;
+	   		var lon = position.coords.longitude;
+	   		console.log("Tu posición es: " + lat + "," + lon);
+	   };
+
+
+})();
